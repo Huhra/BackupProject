aa8e0e4027c21262e6f05d1baece5bce5b818c27
Etienne Magreault <Etienne Magreault>
2017-02-24 00:48:10 +0100

    Initial commit, Watcher seems to be working and start of DB idea

---
+++ BackupProject.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using BackupProject.Core;
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupProject.Console
+{
+    class Program
+    {
+        private static ILog Logger = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            Logger.Info("Starting program");
+            var watcher = new Watcher();
+            Logger.Info("The watch has started");
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ BackupProject.Core/Watcher.cs
@@ -0,0 +1,59 @@
+﻿using log4net;
+using System.IO;
+
+namespace BackupProject.Core
+{
+    public class Watcher
+    {
+        private static ILog Logger = LogManager.GetLogger(typeof(Watcher));
+
+        public Watcher()
+        {
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = @"C:\Users\etien\";
+            /* Watch for changes in LastAccess and LastWrite times, and
+               the renaming of files or directories. */
+            watcher.NotifyFilter =
+                //The name of the file.
+                NotifyFilters.FileName |
+                //The name of the directory.
+                NotifyFilters.DirectoryName |
+                //The attributes of the file or folder.
+                NotifyFilters.Attributes |
+                //The size of the file or folder.
+                NotifyFilters.Size |
+                //The date the file or folder last had anything written to it.
+                NotifyFilters.LastWrite |
+                //The date the file or folder was last opened.
+                NotifyFilters.LastAccess |
+                //The time the file or folder was created.
+                NotifyFilters.CreationTime |
+                //The security settings of the file or folder.
+                NotifyFilters.Security;
+
+            // Only watch text files.
+            watcher.Filter = "*.*";
+            watcher.IncludeSubdirectories = true;
+
+            // Add event handlers.
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Deleted += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnRenamed);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Logger.Info($"{e.ChangeType}: {e.OldFullPath}|{e.OldName} -> {e.FullPath}|{e.Name}");
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Logger.Info($"{e.ChangeType}: {e.FullPath}|{e.Name}");
+        }
+    }
+}
