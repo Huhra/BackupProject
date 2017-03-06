f7a9abf77937f984f4e8edc7c63d25649559ea75
Etienne Magreault <etienne.magreault@storyshaper.io>
2017-03-06 22:01:10 +0100

    Start of progress models

---
+++ Core.Backup/Copy/EndCopyProgress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Backup.Copy
+{
+    public class EndCopyProgress : CopyProgress
+    {
+        public EndCopyProgress(int filesCount, int filesTotalSize, TimeSpan totalTime, bool cancelled = false)
+        {
+            FilesCount = filesCount;
+            FilesTotalSize = filesTotalSize;
+            TotalTime = totalTime;
+            Cancelled = cancelled;
+        }
+
+        public bool Cancelled { get; set; }
+
+        public TimeSpan TotalTime { get; set; }
+
+        public int FilesTotalSize { get; set; }
+
+        public int FilesCount { get; set; }
+    }
+}
---
+++ Core.Backup/Copy/NormalCopyProgress.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Backup.Copy
+{
+    public class NormalCopyProgress : CopyProgress
+    {
+        public int FilesCopied { get; }
+        public int FilesTotal { get; }
+        public TimeSpan RemainingTime { get; }
+
+        public NormalCopyProgress(int filesCopied, int filesTotal, TimeSpan remainingTime)
+        {
+            FilesCopied = filesCopied;
+            FilesTotal = filesTotal;
+            RemainingTime = remainingTime;
+        }
+    }
+}
